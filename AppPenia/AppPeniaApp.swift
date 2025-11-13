fc2aecada087eb4ca84d129a3af8803e540d3fe0
manoloacademia <luispablosegovia@icloud.com>
2025-11-13 11:07:06 -0300

    Initial Commit

---
+++ AppPenia/AppPeniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPeniaApp.swift
+//  AppPenia
+//
+//  Created by Pablo Segovia on 13/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppPeniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPenia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppPenia
+//
+//  Created by Pablo Segovia on 13/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
